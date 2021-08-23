752866d4d74acc8d6fedb4278859d50c72b8a3e1
Joel Brinkley <joel.brinkley@carecorenational.com>
2021-08-23 12:15:27 -0500

    wip: initial hook up of middleware to topic

---
+++ Extensions/NetStreams.ControlCenter/NetStreams.ControlCenter/ControlCenterTelemetryClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using NetStreams.Telemetry;
+
+namespace NetStreams.ControlCenter
+{
+    public class ControlCenterTelemetryClient : INetStreamTelemetryClient
+    {
+        private NetStreamProducer<string, NetStreamsTelemetryEvent> _producer { get; }
+
+        public ControlCenterTelemetryClient(NetStreamProducer<string, NetStreamsTelemetryEvent> producer)
+        {
+            _producer = producer;
+        }
+
+        public async Task SendAsync(NetStreamsTelemetryEvent telemetryEvent, CancellationToken token)
+        {
+            await _producer.ProduceAsync(telemetryEvent.StreamProcessorName, telemetryEvent);
+        }
+    }
+}
---
+++ Extensions/NetStreams.ControlCenter/NetStreams.ControlCenter/NetStreamBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using NetStreams.Configuration;
+using NetStreams.Serialization;
+using NetStreams.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NetStreams.ControlCenter
+{
+    public static class NetStreamBuilderExtensions
+    {
+        public static void SendTelemetryToControlCenter<TKey, TMessage>(this INetStreamConfigurationBuilderContext<TKey, TMessage> builder, Action<ControlCenterTelemetryConfiguration> setup)
+        {
+            var config = new ControlCenterTelemetryConfiguration();
+            setup(config);
+
+            ProducerBuilder<string, NetStreamsTelemetryEvent> producerBuilder = new ProducerBuilder<string, NetStreamsTelemetryEvent>(config.ToProducerConfig())
+                .SetValueSerializer(new HeaderSerializationStrategy<NetStreamsTelemetryEvent>());
+
+            var netStreamProducer = new NetStreamProducer<string, NetStreamsTelemetryEvent>(
+                config.TelemetryTopic,
+                producerBuilder.Build());
+
+            builder.SendTelemetry(new ControlCenterTelemetryClient(netStreamProducer));
+        }
+    }
+}
