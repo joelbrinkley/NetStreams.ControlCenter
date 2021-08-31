21bc8f326c2cc72b86ea48be37816cfd7fab0902
Joel Brinkley <joel.brinkley@carecorenational.com>
2021-08-31 10:52:25 -0500

    cleanup folders

---
+++ src/NetStreams.ControlCenter/Server/NetStreams.ControlCenter.TelemetryProcessor/Infrastructure/EntityFrameworkCore/TelemetryDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetStreams.ControlCenter.TelemetryProcessor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStreams.ControlCenter.TelemetryProcessor.Infrastructure.EntityFrameworkCore
+{
+    public class TelemetryDbContext : DbContext
+    {
+        public DbSet<StreamProcessor> StreamProcessors { get; set; }
+
+        public TelemetryDbContext(DbContextOptions<TelemetryDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StreamProcessor>()
+                .HasKey(model => model.Id);
+
+            modelBuilder.Entity<StreamProcessor>().HasIndex(model => model.Name).IsUnique();
+
+        }
+    }
+}
---
+++ src/NetStreams.ControlCenter/Server/NetStreams.ControlCenter.TelemetryProcessor/Models/StreamProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetStreams.ControlCenter.TelemetryProcessor.Models
+{
+    public class StreamProcessor
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Running { get; set; }
+        public DateTimeOffset LastStarted { get; set; }
+
+        public ICollection<ConsumedMessage> ConsumedMessages { get; set; } = new List<ConsumedMessage>();
+    }
+}
